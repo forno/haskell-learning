ee807d54187a96bd153290c38704a2a99c21bac6
FORNO <forno@xmaho.link>
2023-05-26 01:53:50 +0000

    clear 03-01
    Signed-off-by: FORNO <forno@xmaho.link>

--- hello.hs
+++ hello.hs
@@ -1,5 +1,13 @@
 main = putStrLn myhtml
 
-myhtml = wrapHtml "Hello, world!"
+myhtml = makeHtml "My page title" "My page content"
 
-wrapHtml content = "<html><body>" <> content <> "</body></html>"
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+html_ content = "<html>" <> content <> "></html>"
+
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
