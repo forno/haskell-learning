06e722fb20560025605b1a4e890670402a7bd69e
FORNO <forno@xmaho.link>
2023-05-26 02:03:10 +0000

    Add el function
    Signed-off-by: FORNO <forno@xmaho.link>

--- hello.hs
+++ hello.hs
@@ -1,13 +1,23 @@
+main :: IO ()
 main = putStrLn myhtml
 
+myhtml :: String
 myhtml = makeHtml "My page title" "My page content"
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
-html_ content = "<html>" <> content <> "></html>"
+html_ :: String -> String
+html_ = el "html"
 
-body_ content = "<body>" <> content <> "</body>"
+body_ :: String -> String
+body_ = el "body"
 
-head_ content = "<head>" <> content <> "</head>"
+head_ :: String -> String
+head_ = el "head"
 
-title_ content = "<title>" <> content <> "</title>"
+title_ :: String -> String
+title_ = el "title"
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
